0d27c0495aebb39d8edbd8a99466a86bd1dadc78
Giannis Gkoulioumis <giannis.gkoulioumis@gmail.com>
2021-03-10 21:42:36 +0200

    Add pcg as the random generator

--- source/tools/random-generator.h
+++ source/tools/random-generator.h
@@ -1,16 +1,19 @@
 #pragma once
+#pragma warning(disable:4146)
 
 #include <random>
 #include <chrono>
+#include <pcg/pcg_random.hpp>
+#include <pcg/pcg_extras.hpp>
 
 class RandomGenerator {
 private:
-	std::mt19937 m_randomDevice;
+	pcg32_fast m_randomDevice;
 	std::uniform_real_distribution<float> m_distribution{ std::uniform_real_distribution<float>(0.0f, 0.9999f) };
 
 public:
-	RandomGenerator() noexcept :
-		RandomGenerator(std::chrono::system_clock::now().time_since_epoch().count()) { }
+	RandomGenerator() :
+		m_randomDevice(pcg_extras::seed_seq_from<std::random_device>()) { }
 
 	RandomGenerator(unsigned long long seed) : m_randomDevice(seed) { }
 
